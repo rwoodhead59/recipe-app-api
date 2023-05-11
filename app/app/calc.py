8a79000649a7dc4000b271c6f6895970c992e72f
Richard Woodhead <dr.richard.woodhead@gmail.com>
2023-05-11 09:02:12 -0400

    added migrations

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,11 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """add x and y and return result"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x and y and return result"""
     return y - x
